d5d708c2fe438117367ccf030667ba6cf28b429a
Zacharia Dolal <zacharia.dolal@gmail.com>
2025-01-10 23:44:18 -0600

    feat: Add Docker Compose configuration with PostgreSQL and secure environment variables - Created `docker-compose.yml` to define and manage PostgreSQL as a service. - Mapped port 5432 for database access. - Persisted PostgreSQL data using a local volume (`./database`). - Added `variables.env` file to store PostgreSQL environment variables. - Includes POSTGRES_USER, POSTGRES_PASSWORD, and POSTGRES_DB. - Configured `application.properties` for Spring Boot: - Imported environment variables via `env.properties`. - Configured Spring DataSource using placeholders for PostgreSQL credentials. - Added `env.properties` to securely store database credentials for Spring Boot. - Variables include POSTGRES_USER, POSTGRES_PASSWORD, and POSTGRES_DB. - Ensured sensitive environment files (`variables.env` and `env.properties`) are excluded from version control by adding them to `.gitignore`.

---
+++ src/main/java/com/zacharia/applicatiofybe/ApplicatiofyBeApplication.java
@@ -0,0 +1,13 @@
+package com.zacharia.applicatiofybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicatiofyBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicatiofyBeApplication.class, args);
+    }
+
+}
